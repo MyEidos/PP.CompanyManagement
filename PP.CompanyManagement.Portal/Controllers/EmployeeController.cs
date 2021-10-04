1d11deabc68ed7a5c67afca7ede056eff5714474
Pavlo Parafilo <pavlo.parafilo@stryker.com>
2021-10-05 01:20:56 +0300

    addCLientApp

--- PP.CompanyManagement.Portal/Controllers/EmployeeController.cs
+++ PP.CompanyManagement.Portal/Controllers/EmployeeController.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using NJsonSchema.CodeGeneration.TypeScript;
+using NSwag;
+using NSwag.CodeGeneration.TypeScript;
 using PP.CompanyManagement.Core.Business.Managers;
 using PP.CompanyManagement.Core.Contracts.Dto;
 using PP.CompanyManagement.Portal.ViewModels.Employee;
@@ -51,6 +54,42 @@ namespace PP.CompanyManagement.Portal.Controllers
             var createRes = await this.employeeManager.Update(id, employeeDto);
 
             return this.mapper.Map<UpdateEmployeeResponse>(createRes);
+        }
+
+        [HttpGet("test/{url}")]
+        public async Task<string> Test(string url)
+        {
+            var document = await OpenApiDocument.FromUrlAsync(Uri.UnescapeDataString(url));
+
+            //var settings = new TypeScriptClientGeneratorSettings
+            //{
+            //    ClassName = "{controller}Client",
+            //};
+            //var generator = new TypeScriptClientGenerator(document, settings);
+
+            var settings = new TypeScriptClientGeneratorSettings
+            {
+                GenerateClientClasses = false,
+                GenerateClientInterfaces = false,
+                GenerateOptionalParameters = true
+            };
+
+            var typeGeneratorSettings = new TypeScriptGeneratorSettings
+            {
+                DateTimeType = TypeScriptDateTimeType.String,
+                GenerateConstructorInterface = false,
+                MarkOptionalProperties = true,
+                TypeStyle = TypeScriptTypeStyle.Interface
+            };
+
+            var typeResolver = new TypeScriptTypeResolver(typeGeneratorSettings);
+
+            var generator = new TypeScriptClientGenerator(document, settings, typeResolver);
+
+
+            var code = generator.GenerateFile();
+
+            return code;
         }
     }
 }
